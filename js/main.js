f4aad9dc050bae9c73d7d4b777dff4d3a3a65ef4
Dusan Rajlic <dusan.rajlic1@gmail.com>
2020-09-28 01:47:16 +0200

    initial commit - will add a different GUI and some additional functionality

---
+++ js/main.js
@@ -0,0 +1,175 @@
+import productDB, {
+    bulkCreate,
+    getID,
+    CreateElement
+} from './module.js';
+
+let db = productDB('ProductDB', {
+    products: `++id, name, seller, price`
+});
+
+// GET INPUT ELEMENTS
+
+const userID = document.getElementById('userID');
+const productName = document.getElementById('productName');
+const seller = document.getElementById('seller');
+const price = document.getElementById('price');
+
+// get buttons
+
+const btnCreate = document.getElementById('btnCreate');
+const btnRead = document.getElementById('btnRead');
+const btnUpdate = document.getElementById('btnUpdate');
+const btnDelete = document.getElementById('btnDelete');
+
+
+// insert value with create button
+
+btnCreate.onclick = (event) => {
+    let flag = bulkCreate(db.products, {
+        name: productName.value,
+        seller: seller.value,
+        price: price.value
+    })
+
+    console.log(flag);
+
+    productName.value = seller.value = price.value = '';
+
+    getID(db.products, (data) => {
+        // changing the number of the ID.. if we have already a record in our database, it will be +1.. if we don't, then we specify first
+        userID.value = data.id + 1 || 1;
+    });
+}
+
+
+// read data from database
+
+btnRead.onclick = ReadTable;
+
+// update database 
+
+btnUpdate.onclick = () => {
+    const id = parseInt(userID.value || 0);
+    if (id) {
+        // call dexie update method
+        db.products.update(id, {
+            name: productName.value,
+            seller: seller.value,
+            price: price.value
+        }).then((updated) => {
+            // let get = updated ? `data updated` : `couldn't update data`;
+            let get = updated ? true : false;
+
+            // // display message
+            // let updatemsg = document.querySelector(".updatemsg");
+            // getMsg(get, updatemsg);
+
+            // proname.value = seller.value = price.value = "";
+            // //console.log(get);
+        })
+    } else {
+        console.log(`Please Select id: ${id}`);
+    }
+}
+
+function ReadTable() {
+    const tbody = document.getElementById('tbody');
+
+    // empty out the duplicates 
+
+    while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.firstChild);
+    }
+
+    getID(db.products, (data) => {
+
+        if (data) {
+            CreateElement('tr', tbody, tr => {
+
+                // executes 4 times for all values in data
+                for (const value in data) {
+                    CreateElement('td', tr, td => {
+                        td.textContent = data.price === data[value] ? `€ ${data[value]}` : data[value];
+                    })
+                }
+
+                // append buttons to our current row
+
+                // EDIT button
+                CreateElement('td', tr, td => {
+                    // create i tag
+                    CreateElement('i', td, i => {
+                        i.className += 'fas fa-edit btnEdit'
+
+                        // assign data-id attribute to i icon
+                        i.setAttribute(`data-id`, data.id);
+
+                        // EDIT individual post functionality ..
+                        // copies current values from the database into the active input
+                        i.onclick = EditPost;
+                    })
+                })
+
+                // DELETE button
+                CreateElement('td', tr, td => {
+                    // create i tag
+                    CreateElement('i', td, i => {
+                        i.className += 'fas fa-trash-alt btnTrash'
+
+                        // assign data-id attribute to i icon
+                        i.setAttribute(`data-id`, data.id);
+
+                        i.onclick = DeletePost;
+                    })
+                })
+            })
+        }
+    })
+}
+
+function EditPost(event) {
+    // get the id of the clicked tr, we get a string out.. parse it, get a number
+    let id = parseInt(event.target.dataset.id);
+
+    db.products.get(id, data => {
+        // set the input values to the values of the according ID from database
+
+        userID.value = data.id || 0;
+        productName.value = data.name || "";
+        seller.value = data.seller || "";
+        price.value = data.price || 0;
+    })
+
+}
+
+function DeletePost(event) {
+    // get id 
+    let id = parseInt(event.target.dataset.id);
+
+    // using dexie.js function
+    db.products.delete(id);
+
+    ReadTable();
+}
+
+// delete ALL
+
+btnDelete.onclick = () => {
+    // using dexie.js method to delete the whole database 
+
+    db.delete();
+
+    // created the database again according to the same specifications 
+    db = productDB('ProductDB', {
+        products: `++id, name, seller, price`
+    });
+
+    // open database
+    db.open();
+
+
+    // update table 
+    ReadTable();
+}
+
